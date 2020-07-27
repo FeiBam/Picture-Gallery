525b705393ba2d38d51e00d6cade28aecd6fd983
Fei_Bam <Fly_Boom@girigiri.love>
2020-07-28 01:01:36 +0800

    Update PictureGallery.vue

--- PictureGallery.vue
+++ PictureGallery.vue
@@ -11,13 +11,11 @@
             </template>
         </div>
         <div class="Picture-Gallery-box" v-show="GalleryIsShow">
-            <slot name="head">
-                <div class="Picture-Gallery-info-box flex-center-box">
-                    <h3 style="margin: 0;color: white">{{PictureTitle}}</h3>
-                    <a style="color: white;font-size: x-small">{{PictureInfo}}</a>
-                    <i style="position: absolute;right: 20px;top: 20px;color: white;cursor: pointer" @click="GalleryShowChange()"><svg viewBox="64 64 896 896" data-icon="close" width="1em" height="1em" fill="currentColor" aria-hidden="true" focusable="false" class=""><path d="M563.8 512l262.5-312.9c4.4-5.2.7-13.1-6.1-13.1h-79.8c-4.7 0-9.2 2.1-12.3 5.7L511.6 449.8 295.1 191.7c-3-3.6-7.5-5.7-12.3-5.7H203c-6.8 0-10.5 7.9-6.1 13.1L459.4 512 196.9 824.9A7.95 7.95 0 0 0 203 838h79.8c4.7 0 9.2-2.1 12.3-5.7l216.5-258.1 216.5 258.1c3 3.6 7.5 5.7 12.3 5.7h79.8c6.8 0 10.5-7.9 6.1-13.1L563.8 512z"></path></svg></i>
-                </div>
-            </slot>
+            <div class="Picture-Gallery-info-box flex-center-box">
+                <h3 style="margin: 0;color: white">{{PictureTitle}}</h3>
+                <a style="color: white;font-size: x-small">{{PictureInfo}}</a>
+                <i style="position: absolute;right: 20px;top: 20px;color: white;cursor: pointer" @click="GalleryShowChange()"><svg viewBox="64 64 896 896" data-icon="close" width="1em" height="1em" fill="currentColor" aria-hidden="true" focusable="false" class=""><path d="M563.8 512l262.5-312.9c4.4-5.2.7-13.1-6.1-13.1h-79.8c-4.7 0-9.2 2.1-12.3 5.7L511.6 449.8 295.1 191.7c-3-3.6-7.5-5.7-12.3-5.7H203c-6.8 0-10.5 7.9-6.1 13.1L459.4 512 196.9 824.9A7.95 7.95 0 0 0 203 838h79.8c4.7 0 9.2-2.1 12.3-5.7l216.5-258.1 216.5 258.1c3 3.6 7.5 5.7 12.3 5.7h79.8c6.8 0 10.5-7.9 6.1-13.1L563.8 512z"></path></svg></i>
+            </div>
             <div style="width: 100%;height: 100%">
                 <template v-for="item in PictureObjectArr">
                     <div class="Picture-Gallery-item-box flex-box" :class="[item.isShow ? 'Picture-isShow' : 'Picture-unShow']" :key="item.id">
