6596f7700238727c7d0cf6fb342cae30f629bb87
Fei_Bam <Fly_Boom@girigiri.love>
2020-08-03 07:25:21 +0800

    修复了 响应式盒子模型 超出父盒子的问题
    修复响应式的问题，修复图片没有完全展开的问题

--- PictureGallery.vue
+++ PictureGallery.vue
@@ -3,7 +3,7 @@
         <div class="Picture-box">
             <template v-for="item in PictureObjectArr">
                 <div :key="item.id" class="Picture-item-box">
-                    <img class="Picture" :src="item.picture">
+                    <img  class="Picture" :src="item.picture">
                     <div @click="PictureChange(item.id)" class="Picture-over-box flex-center-box">
                         <h4 class="flex-center-box">点击放大</h4>
                     </div>
@@ -11,11 +11,13 @@
             </template>
         </div>
         <div class="Picture-Gallery-box" v-show="GalleryIsShow">
-            <div class="Picture-Gallery-info-box flex-center-box">
-                <h3 style="margin: 0;color: white">{{PictureTitle}}</h3>
-                <a style="color: white;font-size: x-small">{{PictureInfo}}</a>
-                <i style="position: absolute;right: 20px;top: 20px;color: white;cursor: pointer" @click="GalleryShowChange()"><svg viewBox="64 64 896 896" data-icon="close" width="1em" height="1em" fill="currentColor" aria-hidden="true" focusable="false" class=""><path d="M563.8 512l262.5-312.9c4.4-5.2.7-13.1-6.1-13.1h-79.8c-4.7 0-9.2 2.1-12.3 5.7L511.6 449.8 295.1 191.7c-3-3.6-7.5-5.7-12.3-5.7H203c-6.8 0-10.5 7.9-6.1 13.1L459.4 512 196.9 824.9A7.95 7.95 0 0 0 203 838h79.8c4.7 0 9.2-2.1 12.3-5.7l216.5-258.1 216.5 258.1c3 3.6 7.5 5.7 12.3 5.7h79.8c6.8 0 10.5-7.9 6.1-13.1L563.8 512z"></path></svg></i>
-            </div>
+            <slot name="head">
+                <div class="Picture-Gallery-info-box flex-center-box">
+                    <h3 style="margin: 0;color: white">{{PictureTitle}}</h3>
+                    <a style="color: white;font-size: x-small">{{PictureInfo}}</a>
+                    <i style="position: absolute;right: 20px;top: 20px;color: white;cursor: pointer" @click="GalleryShowChange()"><svg viewBox="64 64 896 896" data-icon="close" width="1em" height="1em" fill="currentColor" aria-hidden="true" focusable="false" class=""><path d="M563.8 512l262.5-312.9c4.4-5.2.7-13.1-6.1-13.1h-79.8c-4.7 0-9.2 2.1-12.3 5.7L511.6 449.8 295.1 191.7c-3-3.6-7.5-5.7-12.3-5.7H203c-6.8 0-10.5 7.9-6.1 13.1L459.4 512 196.9 824.9A7.95 7.95 0 0 0 203 838h79.8c4.7 0 9.2-2.1 12.3-5.7l216.5-258.1 216.5 258.1c3 3.6 7.5 5.7 12.3 5.7h79.8c6.8 0 10.5-7.9 6.1-13.1L563.8 512z"></path></svg></i>
+                </div>
+            </slot>
             <div style="width: 100%;height: 100%">
                 <template v-for="item in PictureObjectArr">
                     <div class="Picture-Gallery-item-box flex-box" :class="[item.isShow ? 'Picture-isShow' : 'Picture-unShow']" :key="item.id">
@@ -24,9 +26,9 @@
                 </template>
             </div>
             <div class="Picture-Gallery-select-box flex-center-box">
-                <i @click="PictureChange(undefined,'next')"><svg viewBox="64 64 896 896" data-icon="left" width="1em" height="1em" fill="currentColor" aria-hidden="true" focusable="false" class=""><path d="M724 218.3V141c0-6.7-7.7-10.4-12.9-6.3L260.3 486.8a31.86 31.86 0 0 0 0 50.3l450.8 352.1c5.3 4.1 12.9.4 12.9-6.3v-77.3c0-4.9-2.3-9.6-6.1-12.6l-360-281 360-281.1c3.8-3 6.1-7.7 6.1-12.6z"></path></svg></i>
+                <i @click="PictureChange(undefined,'previous')"><svg viewBox="64 64 896 896" data-icon="left" width="1em" height="1em" fill="currentColor" aria-hidden="true" focusable="false" class=""><path d="M724 218.3V141c0-6.7-7.7-10.4-12.9-6.3L260.3 486.8a31.86 31.86 0 0 0 0 50.3l450.8 352.1c5.3 4.1 12.9.4 12.9-6.3v-77.3c0-4.9-2.3-9.6-6.1-12.6l-360-281 360-281.1c3.8-3 6.1-7.7 6.1-12.6z"></path></svg></i>
                 <i @click="GallerPreviewIsShow = true"><svg viewBox="64 64 896 896" data-icon="appstore" width="1em" height="1em" fill="currentColor" aria-hidden="true" focusable="false" class=""><path d="M864 144H560c-8.8 0-16 7.2-16 16v304c0 8.8 7.2 16 16 16h304c8.8 0 16-7.2 16-16V160c0-8.8-7.2-16-16-16zm0 400H560c-8.8 0-16 7.2-16 16v304c0 8.8 7.2 16 16 16h304c8.8 0 16-7.2 16-16V560c0-8.8-7.2-16-16-16zM464 144H160c-8.8 0-16 7.2-16 16v304c0 8.8 7.2 16 16 16h304c8.8 0 16-7.2 16-16V160c0-8.8-7.2-16-16-16zm0 400H160c-8.8 0-16 7.2-16 16v304c0 8.8 7.2 16 16 16h304c8.8 0 16-7.2 16-16V560c0-8.8-7.2-16-16-16z"></path></svg></i>
-                <i @click="PictureChange(undefined,'previous')"><svg viewBox="64 64 896 896" data-icon="right" width="1em" height="1em" fill="currentColor" aria-hidden="true" focusable="false" class=""><path d="M765.7 486.8L314.9 134.7A7.97 7.97 0 0 0 302 141v77.3c0 4.9 2.3 9.6 6.1 12.6l360 281.1-360 281.1c-3.9 3-6.1 7.7-6.1 12.6V883c0 6.7 7.7 10.4 12.9 6.3l450.8-352.1a31.96 31.96 0 0 0 0-50.4z"></path></svg></i>
+                <i @click="PictureChange(undefined,'next')"><svg viewBox="64 64 896 896" data-icon="right" width="1em" height="1em" fill="currentColor" aria-hidden="true" focusable="false" class=""><path d="M765.7 486.8L314.9 134.7A7.97 7.97 0 0 0 302 141v77.3c0 4.9 2.3 9.6 6.1 12.6l360 281.1-360 281.1c-3.9 3-6.1 7.7-6.1 12.6V883c0 6.7 7.7 10.4 12.9 6.3l450.8-352.1a31.96 31.96 0 0 0 0-50.4z"></path></svg></i>
             </div>
             <div :class="[GallerPreviewIsShow ? 'Picture-Gallery-preview-box-isShow' : 'Picture-Gallery-preview-box-unShow']" class="Picture-Gallery-preview-box">
                 <div class="Picture-Gallery-preview-info">
@@ -45,24 +47,6 @@
     </div>
 </template>
 <script>
-    Array.prototype.next = function(arrItem){
-      const index = this.indexOf(arrItem)
-      if (index === this.length -1){
-        return this[0]
-      }
-      else {
-        return this[index+1]
-      }
-    }
-    Array.prototype.previous = function(arrItem){
-      const index = this.indexOf(arrItem)
-      if (index === 0){
-        return this[this.length-1]
-      }
-      else {
-        return this[index-1]
-      }
-    }
     export default {
         name: "PictureGallery",
         props:{
@@ -75,7 +59,6 @@
                 PictureObjectArr:[],
                 GalleryIsShow:false,
                 GallerPreviewIsShow:false,
-                PictureShow:false,
                 PictureTitle:'',
                 PictureInfo:'',
             }
@@ -84,7 +67,6 @@
             init:function () {
                 this.PictureArrInit()
             },
-            //初始化对象转换
             PictureArrInit:function () {
                 this.PictureArr.forEach((item,index)=>{
                     let object = {}
@@ -104,7 +86,6 @@
                 })
                 this.PictureObjectArr[0].isShow = true
             },
-            //图片切换方法
             PictureChange:function (index,type) {
                 if (typeof index !== 'undefined'){
                     this.GalleryShowChange('open')
@@ -129,7 +110,6 @@
                     }
                 }
             },
-            //隐藏图片方法
             PictureUnShow:function(){
                 for (let item of this.PictureObjectArr){
                     if (item.isShow){
@@ -138,26 +118,23 @@
                     }
                 }
             },
-            //设置图片介绍方法
             PictureMessageSet:function(title,info){
                 this.PictureTitle = title
                 this.PictureInfo = info
             },
-            //画廊显示切换方法
             GalleryShowChange:function (type = 'close') {
                 if (type === 'open'){
-                    document.body.style.overflow = 'hidden'//固定滚动条 防止显示画廊的时候可以滚动被遮住页面
+                    document.body.style.overflow = 'hidden'
                     this.GalleryIsShow = true
                 }
                 else {
                     this.PictureUnShow()
-                    setTimeout(()=>{ //延时解锁滚动条 延时是为了让图像过度动画完成
+                    setTimeout(()=>{
                         this.GallerPreviewIsShow = false
                         this.GalleryIsShow = false
                         document.body.style.overflow = 'auto'},400)
                 }
             },
-            
         },
         mounted() {
             this.init()
@@ -172,17 +149,19 @@
 <style scoped>
     .Picture-box{
         display: flex;
-        flex-flow: row;
+        flex-flow: wrap;
         width: auto;
         height: auto;
     }
     .Picture-item-box{
         position: relative;
         padding: 8px;
-        width: 33%;
+        width: calc(33% - 16px);
         height: auto;
     }
     .Picture{
+        width: 100%;
+        height: 100%;
         max-width: 100%;
     }
     .Picture-over-box{
@@ -276,6 +255,7 @@
         padding: 8px;
     }
     .Picture-Gallery-select-item-box{
+        flex-flow: wrap;
         padding: 8px;
         background-color: black;
     }
@@ -326,7 +306,7 @@
         .Picture-item-box{
             position: relative;
             padding: 8px;
-            width: 50%;
+            width: calc(50% - 16px);
             height: auto;
         }
     }
@@ -334,7 +314,7 @@
         .Picture-item-box{
             position: relative;
             padding: 8px;
-            width: 33%;
+            width: calc(33% - 16px);
             height: auto;
         }
     }
@@ -342,7 +322,7 @@
         .Picture-item-box{
             position: relative;
             padding: 8px;
-            width: 25%;
+            width: calc(25% - 16px);
             height: auto;
         }
     }
